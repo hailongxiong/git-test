cda7282326c5c327d057431b26b103d4176193e9
hailongxiong <hailongxiong@126.com>
2016-01-21 10:50:52 +0800

    modify test.cc 222222 and add a new file named aa.cc

---
+++ aa.cc
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"It is a test example!\n";
+}
--- test.cc
+++ test.cc
@@ -6,5 +6,6 @@ int main()
     cout<<"Hello world!\n"<<endl;
 
     cout<<"111111";
+    cout<<"222222";
 
 }
